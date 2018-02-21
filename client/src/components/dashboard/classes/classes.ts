d299531c8f932c07ffe1972f13135b48b9ac491a
HaydenHodge <hayden.hodge@scoop-software.cz>
2018-02-21 14:44:20 +0100

    Added basic workflows component

---
+++ client/src/components/dashboard/classes/classes.ts
@@ -0,0 +1,10 @@
+import { Vue, Component} from 'vue-property-decorator';
+import './classes.scss';
+
+@Component({
+    template: require('./classes.html')
+})
+
+export class Classes extends Vue {
+    
+}

74a3c32ce052d729c82362d2720e9d0fd2105c2a
HaydenHodge <hayden.hodge@scoop-software.cz>
2018-03-20 15:43:32 +0100

    Styling new sidebar for multi-engine use

--- client/src/components/dashboard/sidebar/sidebar.ts
+++ client/src/components/dashboard/sidebar/sidebar.ts
@@ -18,10 +18,14 @@ export class SidebarComponent extends Vue {
     drawer = null;
     miniVariant = false;
     settingsShowed = false;
+    host = this.$store.state.connectionSettings.host;
+    port = this.$store.state.connectionSettings.port;
       
     updateTarget(connectionSettings) {
         this.settingsShowed = false;
         this.$store.commit('updateConnectionSettings', connectionSettings);
+        this.host = this.$store.state.connectionSettings.host;
+        this.port = this.$store.state.connectionSettings.port;
         this.$router.push('/dashboard?host=' + this.$store.state.connectionSettings.host + '&port=' + this.$store.state.connectionSettings.port);
     }
 }

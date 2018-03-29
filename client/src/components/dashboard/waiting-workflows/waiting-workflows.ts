b7a9d78818c68aa83625071a99a2e860f87b2622
HaydenHodge <hayden.hodge@scoop-software.cz>
2018-03-29 14:48:49 +0200

    Waiting Workflows component template created and transition animation heights adjusted accordingly

---
+++ client/src/components/dashboard/waiting-workflows/waiting-workflows.ts
@@ -0,0 +1,9 @@
+import { Vue, Component} from 'vue-property-decorator';
+import './waiting-workflows.scss';
+
+@Component({
+    template: require('./waiting-workflows.html')
+})
+export class WaitingWorkflows extends Vue {
+
+}

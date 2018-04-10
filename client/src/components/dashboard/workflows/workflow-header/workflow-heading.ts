5c13cbfcab543e1798c32ecf017d8611464fbe0e
HaydenHodge <hayden.hodge@scoop-software.cz>
2018-04-10 09:55:41 +0200

    Now showing filtered workflow count as heading instead of total workflow count, that information is displayed in the sidebar

--- client/src/components/dashboard/workflows/workflow-header/workflow-heading.ts
+++ client/src/components/dashboard/workflows/workflow-header/workflow-heading.ts
@@ -14,6 +14,7 @@ import './workflow-heading.scss';
 export class WorkflowHeading extends Vue {
     @Prop() engineStatus;
     @Prop() wfType: string;
+    @Prop() wfCount: number;
     @Prop() restartingAll: boolean;
     @Prop() deletingAll: boolean;
     private jmxService: JmxService = this.$services.jmxService;

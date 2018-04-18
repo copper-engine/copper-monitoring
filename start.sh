e6276e9106fffb9852b70cfc62c4ebfbe8e6dcd8
Vladyslav Gutsul <vladyslav.gutsul@scoop-software.cz>
2018-04-18 12:47:55 +0200

    updated start.sh

--- start.sh
+++ start.sh
@@ -17,19 +17,28 @@ then
     echo "---------------------------------------------------------------------------------------------"
     echo ""
     cd "${PATH_TO_SCRIPT}/client" && npm install && npm run build && npm run deploy
-elif [ $1 == '--dockerize' ]
+elif [ $1 == '--prep-docker' ]
 then
     echo ""
-    echo "Will create docker image copper-monitoring"
+    echo "Will prepare files for docker image copper-monitoring"
     echo ""
     echo "---------------------------------------------------------------------------------------------"
     echo ""
-    cd "${PATH_TO_SCRIPT}/client" && npm install && npm run build && npm run deploy && cd ../server && ./gradlew distZip && cp build/distributions/copper-monitoring.zip ../docker/ && cd ../docker && docker build -t copper-monitoring .
+    cd "${PATH_TO_SCRIPT}/client" && npm install && npm run build && npm run deploy && cd ../server && ./gradlew distZip && cp build/distributions/copper-monitoring.zip ../docker/ && cd ../docker
+elif [ $1 == '--dockerize' ]
+then
+    echo ""
+    echo "Will create docker image copper-monitoring from prepared files"
+    echo ""
+    echo "---------------------------------------------------------------------------------------------"
+    echo ""
+    cd ../docker && docker build -t copper-monitoring .
 else 
     echo ""
     echo "Script usage:"
     echo "Run without arguments to start copper monitoring. Will not fetch laters client changes."
     echo "      P.S: use after you runned script with --install parameter."
     echo "--install   : will build client and copy it to server part as a preparation to run the copper-monitoring"
-    echo "--dockerize : will build client & server parts and will prepare a docker image of copper-monitoring"
+    echo "--prep-docker : will build client & server parts and will prepare files for docker image of copper-monitoring"
+    echo "--dockerize : will create docker image of copper-monitoring"
 fi

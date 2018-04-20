e0d61ee3315120a2e0f9c76ddf714c42d29bb386
HaydenHodge <hayden.hodge@scoop-software.cz>
2018-04-20 11:41:01 +0200

    Moment Error now fixed, WF PopUp functioning, and WF color change

--- client/src/components/dashboard/workflows/workflow-details/workflow-details.ts
+++ client/src/components/dashboard/workflows/workflow-details/workflow-details.ts
@@ -2,6 +2,9 @@ import { Vue, Component, Prop} from 'vue-property-decorator';
 import { WorkflowInfo } from '../../../../models/engine';
 import './workflow-details.scss';
 
+import * as moment from 'moment';
+import { create } from 'domain';
+
 @Component({
     template: require('./workflow-details.html')
 })
@@ -16,5 +19,25 @@ export class WorkflowDetails extends Vue {
 
     showSourceCode() {
         this.$emit('showSourceCode', this.workflow);        
+    }
+
+    get creationTS() {
+        if (this.workflow.creationTS !== null) {
+            let creation = moment(this.workflow.creationTS).format('HH:mm DD/MM/YYYY');
+            return creation;
+        } else {
+            return '';        }
+    }
+    get modTS() {
+        if (this.workflow.lastModTS !== null) {
+            return moment(this.workflow.lastModTS).format('HH:mm DD/MM/YYYY');
+        } else {
+            return '';        }
+    }
+    get errorTS() {
+        if (this.workflow.errorData !== null && this.workflow.errorData !== undefined) {
+            return moment(this.workflow.errorData.errorTS).format('HH:mm DD/MM/YYYY');
+        } else {
+            return '';        }
     }
 }

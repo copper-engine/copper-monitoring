c9ad4433603c45bab10894b8838ef974d651ffcc
HaydenHodge <hayden.hodge@scoop-software.cz>
2018-05-14 08:47:01 +0200

    Overview milestone

--- client/src/components/dashboard/overview/overview.ts
+++ client/src/components/dashboard/overview/overview.ts
@@ -10,20 +10,63 @@ export class Overview extends Vue {
     groups: EngineGroup[] = [];
     timeSelect: string[] = ['5 sec', '15 sec', '30 sec', '1 min', '5 min', '15 min'];
     layoutSelect: string[]= ['Row', 'Column'];
-    newTime: string = '';
-    newLayout: string = ''; 
+    newTime: string = '1 min';
+    newLayout: string = 'Row'; 
     openOptions: boolean = false;
+    fetchPeriod: number;
+    fetchInterval: any;
 
     mounted() {
-        this.groups = this.$store.getters.groupsOfEngines;
+        this.getEngines();
+    }
+
+    beforeDestroy() {
+        clearInterval(this.fetchInterval);
     }
 
     get getRow() {
-        if (this.newLayout === 'Row' || this.newLayout === '') {
+        if (this.newLayout === 'Row') {
             return true;
         }
         else {
             return false;
         }
+    }
+
+    updateTime(time: string) {
+        this.newTime = time;
+        this.fetchPeriod = this.getFetch();
+        this.scheduleFetch();
+    }
+
+    getFetch() {
+        let timeSplit = this.newTime.split(' ');
+        let multiplier = 1;
+        if (timeSplit[1] === 'min') {
+            multiplier = 60;
+        }
+        return parseInt(timeSplit[0]) * multiplier;
+    }
+
+    getName(group: EngineGroup) {
+        if (group.engines.length > 1) {
+            return group.name;
+        } else {
+            return group.engines[0].engineId;
+        }
+    }
+
+    scheduleFetch() {
+        if (this.fetchInterval) {
+            clearInterval(this.fetchInterval);
+        }
+        this.getEngines();
+        this.fetchInterval = setInterval(() => {
+            this.getEngines();
+        }, this.fetchPeriod * 1000);
+    }
+
+    getEngines() {
+        this.groups = this.$store.getters.groupsOfEngines;
     }
 }

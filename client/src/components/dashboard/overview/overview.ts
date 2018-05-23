e80d744bf7550549a3c9c82dc08303371994c105
HaydenHodge <hayden.hodge@scoop-software.cz>
2018-05-23 09:52:00 +0200

    More refactoring of Overview data and now graphs react upon user state selection

--- client/src/components/dashboard/overview/overview.ts
+++ client/src/components/dashboard/overview/overview.ts
@@ -61,8 +61,9 @@ export class Overview extends Vue {
     openTelegrafInput: boolean = false;
     openSampleQueries: boolean = false;
     statMap = null;
-    statData: EngineStatData[] = [];
+    chartName: string[] = [];
     states = new ChartStates(true, true, true, true, true, true);
+    chartData: any[] = [];
 
     created() {
         this.timeSelect = this.statisticsService.intervals
@@ -121,14 +122,16 @@ export class Overview extends Vue {
         this.$store.state.user.influx.password = this.password;
     }
 
+    @Watch('states', { deep: true })
     getData() {
         this.groups = this.$store.getters.groupsOfEngines;
         this.statMap =  this.statisticsService.getData(this.currentTimeSelection.time, this.getNames());
-        this.statData = [];
+        this.chartName = [];
+        this.chartData = [];
         this.statMap.forEach((value, key) => {
-            this.statData.push(new EngineStatData(key, value));
+            this.chartName.push(key);
+            this.chartData.push(this.getChartData(this.states, value));
         });
-        this.eventHub.$emit('updateChartData');
     }
 
     getNames() {
@@ -353,4 +356,56 @@ export class Overview extends Vue {
             this.getData();
         }, this.currentTimeSelection.time * 1000);
     }
+
+    getChartData(states: ChartStates, statesPrint: StatesPrint[]) {
+        let dataset = [];
+        if (statesPrint) {
+            if (states.running) {
+                dataset.push({
+                    label: 'RUNNING',
+                    backgroundColor: '#41ce00c4', // green
+                    data: statesPrint.map((state) => state ? state.running : 0)
+                });
+            }
+            if (states.waiting) {
+                dataset.push({
+                    label: 'WAITING',
+                    backgroundColor: '#e4c200de', // yellow
+                    data: statesPrint.map((state) => state ? state.waiting : 0)
+                });
+            }
+            if (states.finished) {
+                dataset.push({
+                    label: 'FINISHED',
+                    backgroundColor: '#1ad8b9c4',  // grey
+                    data: statesPrint.map((state) => state ? state.finished : 0)
+                });
+            }
+            if (states.dequeued) {
+                dataset.push({
+                    label: 'DEQUEUED',
+                    backgroundColor: '#0b7babc4',  // blue
+                    data: statesPrint.map((state) => state ? state.dequeued : 0)
+                });
+            }
+            if (states.error) {
+                dataset.push({
+                    label: 'ERROR',
+                    backgroundColor: '#de1515c4',  // red
+                    data: statesPrint.map((state) => state ? state.error : 0)
+                });
+            }
+            if (states.invalid) {
+                dataset.push({
+                    label: 'INVALID',
+                    backgroundColor: '#770202c4',  // dark red
+                    data: statesPrint.map((state) => state ? state.invalid : 0)
+                });
+            }
+        }
+        return {
+            labels: statesPrint ? statesPrint.map((state) => state ? (Vue as any).moment(state.time).format('HH:mm:ss') : 'NA') : [],
+            datasets: dataset
+        };
+  }
 }
